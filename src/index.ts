0bdc1ad380792ff2ffb7953a78ad2900a5cda7b9
Kevin Rice <kevinrice@Kevins-MacBook-Pro.local>
2019-10-08 14:48:00 -0500

    typed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-class SuiteTooth{
-    callOnMe(name: string){
-        console.log(`Hello, ${name}`); 
-    }
+import { AssertionSuite, SimpleAssertion } from 'sfdx-ftw-assertions'; 
+
+class SuiteTooth extends AssertionSuite{
+    dependencies = ['dependency1', 'dependency2']; 
+    getAssertions(){
+        return [ 
+            simple
+        ];
+    }   
+}
+
+const simple: SimpleAssertion = {
+    apexAssertion: 'system.assert(true)', 
+    description: 'Assert true', 
+    failureCallback: globalCb
+}
+
+function globalCb(result: string): void{
+    console.log(result); 
 }
 
 module.exports = new SuiteTooth(); 
