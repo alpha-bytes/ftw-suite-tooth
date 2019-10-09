181c67066312f1239e01714672e379944e3dfd88
Kevin Rice <kevinrice@Kevins-MacBook-Pro.local>
2019-10-09 13:53:35 -0500

    es3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sfdx_ftw_assertions_1 = require("sfdx-ftw-assertions");
-class SuiteTooth extends sfdx_ftw_assertions_1.AssertionSuite {
-    constructor() {
-        super(...arguments);
-        this.dependencies = ['dependency1', 'dependency2'];
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+var sfdx_ftw_assertions_1 = require("sfdx-ftw-assertions");
+var SuiteTooth = /** @class */ (function (_super) {
+    __extends(SuiteTooth, _super);
+    function SuiteTooth() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.dependencies = ['dependency1', 'dependency2'];
+        return _this;
     }
-    getAssertions() {
+    SuiteTooth.prototype.getAssertions = function () {
         return [
             simple
         ];
-    }
-}
-const simple = {
+    };
+    return SuiteTooth;
+}(sfdx_ftw_assertions_1.AssertionSuite));
+var simple = {
     apexAssertion: 'system.assert(true)',
     description: 'Assert true',
     failureCallback: globalCb
